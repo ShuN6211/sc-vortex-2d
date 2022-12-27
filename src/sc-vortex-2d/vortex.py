467fc91d40feb9efe7a7cb8dd137a1c169128377
Shun Makino <shunmakino6211@gmail.com>
2022-12-27 23:29:36 +0900

    add vortex.py

---
+++ src/sc-vortex-2d/vortex.py
@@ -0,0 +1,25 @@
+from vortex_instance import VortexInstance
+
+
+class VortexInstanceT03(VortexInstance):
+    pass
+
+
+def cdgm_t03(x: float):
+    pass
+
+
+class VortexInstanceT05(VortexInstance):
+    pass
+
+
+def cdgm_t05(x: float):
+    pass
+
+
+class VortexInstanceT08(VortexInstance):
+    pass
+
+
+def cdgm_t08(x: float):
+    pass
