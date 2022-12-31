c8f20b6f385b4fd26843dac4e834bea182b63de2
Shun Makino <shunmakino6211@gmail.com>
2023-01-01 05:29:30 +0900

    make test05 and test08

--- tests/test_vortex_instance_t05.py
+++ tests/test_vortex_instance_t05.py
@@ -1,35 +1,35 @@
 import pytest
 from scipy import interpolate
 
-from src.sc_vortex_2d.vortex import VortexInstanceT08
+from src.sc_vortex_2d.vortex import VortexInstanceT05
 
 
 @pytest.fixture
-def make_instance() -> VortexInstanceT08:
-    instance = VortexInstanceT08()
+def make_instance() -> VortexInstanceT05:
+    instance = VortexInstanceT05()
     instance.construct()
     return instance
 
 
 def test_instance(make_instance):
-    instance: VortexInstanceT08 = make_instance
+    instance: VortexInstanceT05 = make_instance
     assert len(instance.spectra_dict) == 140
     assert len(instance.u_dict) == 140
     assert len(instance.v_dict) == 140
 
 
 def test_get_pair_potential(make_instance):
-    instance: VortexInstanceT08 = make_instance
+    instance: VortexInstanceT05 = make_instance
     assert isinstance(instance.get_pair_potential(), interpolate.CubicSpline)
 
 
 def test_get_ith_eigen_energy(make_instance):
-    instance: VortexInstanceT08 = make_instance
+    instance: VortexInstanceT05 = make_instance
     assert instance.get_ith_eigen_energy(0) == 0.5
     assert instance.get_ith_eigen_energy(-1) == -0.5
 
 def test_get_ith_eigen_func(make_instance):
-    instance: VortexInstanceT08 = make_instance
+    instance: VortexInstanceT05 = make_instance
     target = instance.get_ith_eigen_func(0)
     assert len(target) == 2
     assert isinstance(target[0], interpolate.CubicSpline)
