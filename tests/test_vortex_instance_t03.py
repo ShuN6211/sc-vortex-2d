2efa059fe38943fcd00c94c1068a3a0a0a342f4a
Shun Makino <shunmakino6211@gmail.com>
2023-01-01 04:41:56 +0900

    make test ver1

--- tests/test_vortex_instance_t03.py
+++ tests/test_vortex_instance_t03.py
@@ -1,15 +1,37 @@
 import pytest
-from src.sc_vortex_2d.vortex import VortexInstanceT03
 from scipy import interpolate
+
+from src.sc_vortex_2d.vortex import VortexInstanceT03
 
 
 @pytest.fixture
 def make_instance() -> VortexInstanceT03:
     instance = VortexInstanceT03()
     instance.construct()
-    return VortexInstanceT03()
+    return instance
+
+
+def test_instance(make_instance):
+    instance: VortexInstanceT03 = make_instance
+    assert len(instance.spectra_dict) == 140
+    assert len(instance.u_dict) == 140
+    assert len(instance.v_dict) == 140
 
 
 def test_get_pair_potential(make_instance):
     instance: VortexInstanceT03 = make_instance
     assert isinstance(instance.get_pair_potential(), interpolate.CubicSpline)
+
+
+def test_get_ith_eigen_energy(make_instance):
+    instance: VortexInstanceT03 = make_instance
+    assert instance.get_ith_eigen_energy(0) == 0.5
+    assert instance.get_ith_eigen_energy(-1) == -0.5
+
+def test_get_ith_eigen_func(make_instance):
+    instance: VortexInstanceT03 = make_instance
+    target = instance.get_ith_eigen_func(0)
+    assert len(target) == 2
+    assert isinstance(target[0], interpolate.CubicSpline)
+    assert isinstance(target[1], interpolate.CubicSpline)
+    
